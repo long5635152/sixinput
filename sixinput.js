38b44c2eaea413cb701b6c776c1152a22dd7d844
zhangchenglong <595105762@qq.com>
2018-09-17 15:46:15 +0800

    新增配置选项,option.type,可选值:"text"和"password",text时文本框显示明文,password时文本框显示圆点. 更新了文字的删除方式,只能从最后一位填写的文字开始删除,直接按"backspace"或"delete"键就可以删除

--- sixinput.js
+++ sixinput.js
@@ -11,75 +11,105 @@ function Sixinput (ele, option, callback) {
     fontSize: 14, // input内字体大小
     align: "center", // input内对齐方式
     color: "#000", // input内字体颜色
-    backgroundColor: "#fff" // input背景色
+    backgroundColor: "#fff", // input背景色
+    type: "password" //类型
   }
   Object.assign(this.options, option)
   this.init();
 }
 
-Sixinput.prototype.init = function () {
-  var This = this;
-  for (var i = 1;i <= this.options.count; i++){
-    $("<input class='inputItem"+ i +"' type='text'>").val("").css({"textAlign": this.options.align, "color": this.options.color, "width": this.options.width + "px", "height": this.options.height + "px","lineHeight": this.options.height + "px", "margin": this.options.margin + "px", "fontSize": this.options.fontSize + "px", "backgroundColor": this.options.backgroundColor}).appendTo(this.ele).get(0).addEventListener("input", function () {
-      if (!this.value.match(/\d/)) {
-        this.value = "";
-      }
-      if (this.value.length > 1) {
-        this.value = this.value.slice(0, 1);
-      }
-    })
-  }
-  $(This.ele).children("input").each(function (index, item) {
-    item.oninput = function () {
-      if ($(this).val() != "") {
-        if ($(this).next().get(0)) {
-          $(this).next().get(0).focus()
-        }
-      } else {
-        if ($(this).prev().get(0)) {
-          $(this).prev().get(0).focus()
+Sixinput.prototype = {
+  init: function () {
+    var This = this;
+    for (var i = 1;i <= this.options.count; i++){
+      $("<input class='inputItem"+ i +"' type='text'>").val("").css({"textAlign": this.options.align, "color": this.options.color, "width": this.options.width + "px", "height": this.options.height + "px","lineHeight": this.options.height + "px", "margin": this.options.margin + "px", "fontSize": this.options.fontSize + "px", "backgroundColor": this.options.backgroundColor}).appendTo(this.ele)
+    }
+    $(This.ele).children("input").each(function (index, item) {
+      item.onkeydown = function (e) {
+        if (e.keyCode == 8 || e.keyCode == 46) {
+          if ($(this).val() == "") {
+            $(this).prev().val("");
+            $(this).prev().trigger("focus", true);
+          } else {
+            $(this).val("")
+          }
         }
       }
-      This.finalVal()
-    }
-  })
-}
-Sixinput.prototype.finalVal = function () {
-  var This = this
-  var ok = true
-  $(this.ele).children("input").each(function (index, item) {
-    if (item.value == "") {
-      $(This.ele).removeAttr("value")
-      ok = false;
-    }
-  })
-  if (ok) {
-    var value = "";
-    $(this.ele).children("input").each(function (index, item) {
-      value += $(item).val()
+      item.oninput = function (e) {
+        if ($(this).val() != "") {
+          if (!this.value.match(/\d/) && this.value.length == 1) {
+            this.value = "";
+            return;
+          }
+          if (this.value.length > 1) {
+            this.value = this.value.replace(e.data, "");
+          } else {
+            $(this).data("value", e.data)
+          }
+          if (This.options.type == "password") {
+            this.value = "●";
+          } else if (This.options.type == "text") {}
+  
+          if ($(this).next().length >= 1) {
+            $(this).next().get(0).focus()
+          }
+        }
+        This.finalVal()
+      }
+      item.onfocus = function (e, isEnd) {
+        if (isEnd) return;
+        var emptyindex = null;
+        for (var i = 0; i < $(This.ele).children("input").length; i++) {
+          if ($(This.ele).children("input").eq(i).val() == "") {
+            emptyindex = i;
+            break;
+          }
+        }
+        if (emptyindex == null) {
+          $(This.ele).children("input").last().trigger("focus", true);
+        } else {
+          $(This.ele).children("input").eq(emptyindex).trigger("focus", true);
+        }
+      }
     })
-    $(this.ele).attr("value", value)
-    if (this.callback) {
-      this.callback(value)
+  },
+  finalVal: function () {
+    var This = this
+    var ok = true
+    $(this.ele).children("input").each(function (index, item) {
+      if (item.value == "") {
+        $(This.ele).removeAttr("value")
+        ok = false;
+      }
+    })
+    if (ok) {
+      var value = "";
+      $(this.ele).children("input").each(function (index, item) {
+        value += $(item).data("value");
+      })
+      $(this.ele).attr("value", value)
+      if (this.callback) {
+        this.callback(value)
+      }
     }
+  },
+  clear: function () {
+      $(this.ele).children("input").each(function (index, item) {
+        $(item).val("")
+      })
+      $(this.ele).removeAttr("value")
+  },
+  onlyread: function (color) {
+      $(this.ele).children("input").each(function (index, item) {
+          $(item).attr("disabled", "disabled");
+          $(item).css("backgroundColor", color ? color :"#eee");
+      })
+  },
+  writeable: function () {
+    var This = this;
+      $(this.ele).children("input").each(function (index, item) {
+          $(item).removeAttr("disabled");
+          $(item).css("backgroundColor", This.options.backgroundColor);
+      })
   }
-}
-Sixinput.prototype.clear = function () {
-    $(this.ele).children("input").each(function (index, item) {
-      $(item).val("")
-    })
-    $(this.ele).removeAttr("value")
-}
-Sixinput.prototype.onlyread = function (color) {
-    $(this.ele).children("input").each(function (index, item) {
-        $(item).attr("disabled", "disabled");
-        $(item).css("backgroundColor", color);
-    })
-}
-Sixinput.prototype.writeable = function () {
-  var This = this;
-    $(this.ele).children("input").each(function (index, item) {
-        $(item).removeAttr("disabled");
-        $(item).css("backgroundColor", This.options.backgroundColor);
-    })
 }
